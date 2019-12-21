1f02ef686fe3e9e09744bcf53bd940877256ca1b
Fibrizo <davidvarela87@gmail.com>
2019-12-21 16:59:51 +0100

    add mail and null

--- API/core/controllers/account/create-account-controller.js
+++ API/core/controllers/account/create-account-controller.js
@@ -6,42 +6,64 @@ const jwt = require('jsonwebtoken')
 
 //Recibir datos y dar de alta usuario
 /**
- *
  * @param {*} req
  * @param {*} res
  * @param {*} next
  */
 
 async function createAccountController(req, res, next) {
-  //Comprobar req.body = null; Y si el cliente es repetido.
+ // Y si el cliente es repetido.
   const accountData = { ...req.body };
-  const securePassword = await bcrypt.hash(accountData.password, 10);
-  try {
-    const newCostumer = await customers.create({
-      name: accountData.name,
-      password: securePassword,
-      mail: accountData.email,
-      phone: accountData.phone
+  const condition = ((accountData != null) && (accountData.name != null)) // Y demas condiciones
+  
+  if (condition) {
+    //Obtener usuario segun el EMAIL accountData.email
+    const existUser = await customers.findAll({
+      where : {
+        email : accountData.email,
+      }
+    }).then( response => {
+      //La respuesta la compruebas si tiene datos
+      if (response != null){
+        //Si respuesta con datos devolver un error
+        return res.status(418).send(error.message);
+      } else {
+        const securePassword = await bcrypt.hash(accountData.password, 10);
+        //Si no tiene datos das de alta
+        try {
+          const newCostumer = await customers.create({
+            name: accountData.name,
+            password: securePassword,
+            mail: accountData.email,
+            phone: accountData.phone ? accountData.phone : 123456789
+          })
+
+          ///TIENE QUE IR EN UN MIDDLEWARE
+          const payloadJwt = {
+            name: accountData.name,
+            email: accountData.email,
+            role: 'admin',
+          };
+      
+          const jwtExpiresIn = parseInt(process.env.AUTH_ACCESS_TOKEN_TTL);
+          const token = jwt.sign(payloadJwt, process.env.AUTH_JWT_SECRET, {
+            expiresIn: jwtExpiresIn,
+          });
+      
+          const response = {
+            accessToken: token,
+            expiresIn: jwtExpiresIn,
+          };
+          //////////////////
+
+          return res.status(201).send(response);
+        } catch (error) {
+          return res.status(500).send(error.message);
+        }
+      }
     })
-    const payloadJwt = {
-      name: accountData.name,
-      email: accountData.email,
-      role: 'admin',
-    };
-
-    const jwtExpiresIn = parseInt(process.env.AUTH_ACCESS_TOKEN_TTL);
-    const token = jwt.sign(payloadJwt, process.env.AUTH_JWT_SECRET, {
-      expiresIn: jwtExpiresIn,
-    });
-
-    const response = {
-      accessToken: token,
-      expiresIn: jwtExpiresIn,
-    };
-    return res.status(201).send(response);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(error.message)
+  } else {
+    return res.status(418).send(error.message);
   }
 
 }
