5b81db759c20cd8877dac5f207a722043e8b27af
AlbertoRo <albertorori@yahoo.es>
2019-12-21 19:42:08 +0100

    registro mail único

--- API/core/controllers/account/create-account-controller.js
+++ API/core/controllers/account/create-account-controller.js
@@ -12,21 +12,21 @@ const jwt = require('jsonwebtoken')
  */
 
 async function createAccountController(req, res, next) {
- // Y si el cliente es repetido.
+  // Y si el cliente es repetido.
   const accountData = { ...req.body };
   const condition = ((accountData != null) && (accountData.name != null)) // Y demas condiciones
-  
+
   if (condition) {
     //Obtener usuario segun el EMAIL accountData.email
     const existUser = await customers.findAll({
-      where : {
-        email : accountData.email,
+      where: {
+        mail: accountData.email,
       }
-    }).then( response => {
+    }).then(async (response) => {
       //La respuesta la compruebas si tiene datos
-      if (response != null){
+      if (response.length > 0) {
         //Si respuesta con datos devolver un error
-        return res.status(418).send(error.message);
+        return res.status(418).send();
       } else {
         const securePassword = await bcrypt.hash(accountData.password, 10);
         //Si no tiene datos das de alta
@@ -44,12 +44,12 @@ async function createAccountController(req, res, next) {
             email: accountData.email,
             role: 'admin',
           };
-      
+
           const jwtExpiresIn = parseInt(process.env.AUTH_ACCESS_TOKEN_TTL);
           const token = jwt.sign(payloadJwt, process.env.AUTH_JWT_SECRET, {
             expiresIn: jwtExpiresIn,
           });
-      
+
           const response = {
             accessToken: token,
             expiresIn: jwtExpiresIn,
@@ -63,7 +63,7 @@ async function createAccountController(req, res, next) {
       }
     })
   } else {
-    return res.status(418).send(error.message);
+    return res.status(418).send();
   }
 
 }
