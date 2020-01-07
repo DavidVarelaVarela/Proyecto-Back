077f6d6ff555805de8f3d33a777fc24e63a2211c
AlbertoRo <albertorori@yahoo.es>
2020-01-07 21:18:17 +0100

    enviados datos de pedido para factura

--- API/core/controllers/bill/get-bill-controller.js
+++ API/core/controllers/bill/get-bill-controller.js
@@ -1,22 +1,34 @@
 "use strict";
 
 const bill = require("../../database/models/bill");
+const products = require("../../database/models/products");
+
 
 async function getBill(req, res, next) {
-   if (parseInt(req.params.id)){
-    const idBill = await bill.findAll({
-        where:{
-            idOrder:req.params.id
+    products.belongsTo(bill, { targetKey: 'idProduct', foreignKey: 'idProduct' });
+    if (parseInt(req.params.id)) {
+        const idBill = await products.findAll({
+            include: [{
+                model: bill,
+                where: {
+                    idOrder: req.params.id
+                }
+            }]
+        });
+        if (idBill === undefined || idBill.length == 0) {
+            return res.status(404).send("Bill not found")
         }
-        
-    });
-    if (idBill === undefined || idBill.length == 0 ){
-        return res.status(404).send("Bill not found")
-    } 
-    return res.status(200).send(idBill);
-   }
-  
-  return res.status(400).send("Bill wrong query");
+        console.log(idBill);
+        const listOfIdProducts = idBill.map((item) => {
+            return { idProduct: item.idProduct, name: item.name, price: item.price, quantity: item.BILL.quantity }
+        })
+
+
+
+        return res.status(200).send(listOfIdProducts);
+    }
+
+    return res.status(400).send("Bill wrong query");
 }
 
 module.exports = getBill;
